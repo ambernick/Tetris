dc195f6db8e990be7d6b647dde642cdcb0b84ad2
ambernick <ambernich2@yahoo.com>
2022-06-02 14:21:51 -0400

    fixed grammar/spacing error, added other tetrominos to the array

--- app.js
+++ app.js
@@ -12,7 +12,37 @@ document.addEventListener('DOMContentLoaded', () => {
        [1, width+1, width*2+1, width*2],
        [width, width*2, width*2+1, width*2+2]
     ]
+//change rest of these arrays
+    const zTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+     ]
 
-    const the Tetrominoes =[lTetromino]
+     const tTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+     ]
 
+     const oTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+     ]
+
+     const iTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+     ]
+    const theTetrominoes =[lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+    
+    let currentPosition = 4
+    let current = theTetrominoes[0][0]
+ 
 })
